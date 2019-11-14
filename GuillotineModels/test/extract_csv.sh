7801503b4cbbb5572a93e7ffce17e58f261f14b8
Henrique Becker <henriquebecker91@gmail.com>
2019-11-14 19:33:46 -0300

    Changed extract_csv script to make distinction between time spent creating structures to be made into the model and in creating the model with calls to the solver.

--- GuillotineModels/test/extract_csv.sh
+++ GuillotineModels/test/extract_csv.sh
@@ -8,7 +8,7 @@ fi
 logfile="$1"
 
 csv=""
-header="instfname;seed;n;n_;p_;time_to_build_model;time_to_solve_model;num_vars;num_constrs;num_plates;num_picuts;num_cuts_made;obj_value;obj_bound;stop_code"
+header="instfname;seed;n;n_;p_;time_to_enumerate_plates;time_to_solver_build;time_to_build_model;time_to_solve_model;num_vars;num_constrs;num_plates;num_picuts;num_cuts_made;obj_value;obj_bound;stop_code"
 IFS=';' read -ra aheader <<< "$header"
 for f in "${aheader[@]}"; do
 	column=`grep "^$f = " "$logfile" | cut -f 3- -d\  `
