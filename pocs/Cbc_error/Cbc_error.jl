a75b413c4de16231b145c6e047f43a15eee3f483
Henrique Becker <henriquebecker91@gmail.com>
2020-02-27 15:55:08 -0300

    Updated the Cbc error POC to wrap the code into a method.

--- pocs/Cbc_error/Cbc_error.jl
+++ pocs/Cbc_error/Cbc_error.jl
@@ -1,16 +1,21 @@
 #!/usr/bin/env julia --project=@.
 
-#using JuMP, Cbc
-using JuMP, GLPK
+using JuMP, Cbc
+#using JuMP, GLPK
 
-#m = Model(Cbc.Optimizer)
-m = Model(GLPK.Optimizer)
+function run()
+	m = Model(Cbc.Optimizer)
+	#m = Model(GLPK.Optimizer)
 
-empty_terms = AffExpr()
-@constraint(m, empty_terms <= 2)
-optimize!(m)
-@show JuMP.primal_status(m)
-if JuMP.primal_status(m) == MOI.FEASIBLE_POINT
-	@show JuMP.objective_value(m)
+	empty_terms = AffExpr()
+	@constraint(m, empty_terms <= 2)
+	optimize!(m)
+	@show JuMP.primal_status(m)
+	if JuMP.primal_status(m) == MOI.FEASIBLE_POINT
+		@show JuMP.objective_value(m)
+	end
 end
+
+@time run()
+@time run()
 
