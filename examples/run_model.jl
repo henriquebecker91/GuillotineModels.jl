3ec846996c2d714c81fab036c4caf69780b365ed
Henrique Becker <henriquebecker91@gmail.com>
2020-03-25 23:42:56 -0300

    Update Manifest and shebang in examples too.

--- examples/run_model.jl
+++ examples/run_model.jl
@@ -1,4 +1,8 @@
-#!/usr/bin/env julia
+#!/bin/bash
+# -*- mode: julia -*-
+#=
+exec julia --project=@. --color=yes --startup-file=no -e "include(popfirst!(ARGS))" "${BASH_SOURCE[0]}" "$@"
+=#
 
 #import Pkg
 #Pkg.activate(".")
