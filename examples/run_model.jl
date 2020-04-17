04864ce8c60f604f98331cd3ed7265f9de9257ca
Henrique Becker <henriquebecker91@gmail.com>
2020-04-16 22:48:17 -0300

    Updated dependencies. Added new line after examples/run_model.jl (was trashing the terminal).

--- examples/run_model.jl
+++ examples/run_model.jl
@@ -19,7 +19,7 @@ import TimerOutputs
 # just a subset of such solvers installed).
 
 # Supported solvers as of Tue 28 Jan 2020 02:06:06 AM -03:
-import CPLEX
+#import CPLEX
 #import Gurobi
 #import Cbc
 import GLPK
@@ -32,4 +32,5 @@ import GLPK
 
 GuillotineModels.CommandLine.run()
 TimerOutputs.print_timer() # global timer used
+println()
 
